fd35eebed2d04b0f2b5fe03eed16ae7ff298ad7f
Martin Noring <martin.noring@gmail.com>
2024-02-28 21:21:55 +0100

    temp1 chart

--- server/temperatures.php
+++ server/temperatures.php
@@ -22,6 +22,7 @@
     <body>
         <canvas id="chartAmbientTemp"  width="800" height="450"></canvas>
         <canvas id="chartAmbientHum"  width="800" height="450"></canvas>
+        <canvas id="Temp1"  width="800" height="450"></canvas>
 
         <script>
             new Chart(document.getElementById("chartAmbientTemp"), {
@@ -147,91 +148,71 @@
                 }
             }
             });
-        </script>
-<!-- 
-        <script>
-        window.onload = function () {
-        
-        var chartAmbientTemp = new Chart(document.getElementById("chartAmbientTemp"), {
-        	animationEnabled: true,
-	        zoomEnabled: true,
-        	options: {
-                title: {
-                    display: true,
-                    text: 'Lufttemperatur'
-                }
-            },
-            type: 'area',
-        	data: {
+
+            new Chart(document.getElementById("Temp1"), {
+            type: 'line',
+            data: {
                 labels: <?php
                     echo json_encode(array_map(function ($p)
                     {
                         return $p['timestamp'];
                     }, $data));
                     ?>,
-                datasets: [{
-                    fill: '+2',
-                    dataPoints: <?php
+                datasets: [
+                    {
+                        label: "P5",
+                        data: <?php
+                            echo json_encode(array_map(function ($p)
+                            {
+                                return $p['json']['DS'][0]['P5'];
+                            }, $data));
+                    ?>,
+                        fill: '2',
+                        borderColor: "red",
+                        backgroundColor: "rgba(179,181,198,0.5)"
+                    },
+                    {
+                        label: "P50",
+                        data: <?php
                     echo json_encode(array_map(function ($p)
                     {
-                        return $p['json']['ATP5'];
+                        return $p['json']['DS'][0]['P50'];
                     }, $data));
-                    ?>
+                    ?>,
+                        borderColor: "green",
+                        backgroundColor: "rgba(26,181,53,0.5)",
+                        fill: false
+                    },
+                    {
+                        label: "P95",
+                        data: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['json']['DS'][0]['P95'];
+                    }, $data));
+                    ?>,
+                        borderColor: "blue",
+                        backgroundColor: "rgba(27,42,198,0.5)",
+                        fill: false
+                    }
+                ]
+            },
+            options: {
+                legend: { display: true },
+                title: {
+                    display: true,
+                    text: 'Temperatur 1'
                 },
-                {
-        	    	dataPoints: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['json']['ATP50'];
-                    }, $data));
-                    ?>
-        	    },
-                {
-                    dataPoints: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['json']['ATP95'];
-                    }, $data));
-                    ?>
+                elements: {
+                    line: {
+                        tension: 0
+                    }
                 }
-            ]
-        }
-        });
+            }
+            });
 
-        // chartAmbientTemp.render();
+        </script>
 
-        var chartAmbientHum = new Chart(document.getElementById("chartAmbientHum"), {
-        	animationEnabled: true,
-	        zoomEnabled: true,
-        	options: {
-                title: {
-                    display: true,
-                    text: 'Luftfuktighet'
-                }
-            },
-        	data: {
-                type: "line",
-                labels: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['timestamp'];
-                    }, $data));
-                    ?>,
-        		datasets: [{
-                <?php
-                echo json_encode(array_map(function ($p)
-                {
-                    return $p['json']['AHP50'];
-                }, $data));
-                ?>
-                }]
-        	}
-        });
-
-        // chartAmbientHum.render();
-
-        }
-    </script> -->
     </body>
 
 </html>
