b24171a980952d5c533ce316ad7d25d1c3dfcc91
Martin Noring <martin.noring@gmail.com>
2024-02-28 17:15:13 +0100

    new chart

--- server/temperatures.php
+++ server/temperatures.php
@@ -16,13 +16,77 @@
         <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
       </head>
 
     <body>
         <canvas id="chartAmbientTemp"  width="800" height="450"></canvas>
-        <canvas id="chartAmbientHum"  width="800" height="450"></canvas>
+        <!-- <canvas id="chartAmbientHum"  width="800" height="450"></canvas> -->
 
+        <script>
+            new Chart(document.getElementById("chartAmbientTemp"), {
+            type: 'line',
+            data: {
+                labels: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['timestamp'];
+                    }, $data));
+                    ?>,
+                datasets: [
+                    {
+                        label: "P5",
+                        data: <?php
+                            echo json_encode(array_map(function ($p)
+                            {
+                                return $p['json']['ATP5'];
+                            }, $data));
+                    ?>,
+                        fill: '2',
+                        borderColor: "red",
+                        backgroundColor: "rgba(179,181,198,0.5)"
+                    },
+                    {
+                        label: "P50",
+                        data: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['json']['ATP50'];
+                    }, $data));
+                    ?>,
+                        borderColor: "green",
+                        backgroundColor: "rgba(26,181,53,0.5)",
+                        fill: false
+                    },
+                    {
+                        label: "P95",
+                        data: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['json']['ATP95'];
+                    }, $data));
+                    ?>,
+                        borderColor: "blue",
+                        backgroundColor: "rgba(27,42,198,0.5)",
+                        fill: false
+                    }
+                ]
+            },
+            options: {
+                legend: { display: true },
+                title: {
+                    display: true,
+                    text: 'Blaj'
+                },
+                elements: {
+                    line: {
+                        tension: 0
+                    }
+                }
+            }
+            });
+        </script>
+<!-- 
         <script>
         window.onload = function () {
         
@@ -105,7 +169,7 @@
         // chartAmbientHum.render();
 
         }
-    </script>
+    </script> -->
     </body>
 
 </html>
