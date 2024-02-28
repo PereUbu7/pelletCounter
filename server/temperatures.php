b3cdcc0c9bca5fdec9d8da1b1389e2acbac95bc0
Martin Noring <martin.noring@gmail.com>
2024-02-28 21:18:20 +0100

    air humidity chart

--- server/temperatures.php
+++ server/temperatures.php
@@ -21,7 +21,7 @@
 
     <body>
         <canvas id="chartAmbientTemp"  width="800" height="450"></canvas>
-        <!-- <canvas id="chartAmbientHum"  width="800" height="450"></canvas> -->
+        <canvas id="chartAmbientHum"  width="800" height="450"></canvas>
 
         <script>
             new Chart(document.getElementById("chartAmbientTemp"), {
@@ -76,7 +76,69 @@
                 legend: { display: true },
                 title: {
                     display: true,
-                    text: 'Blaj'
+                    text: 'Lufttemperatur'
+                },
+                elements: {
+                    line: {
+                        tension: 0
+                    }
+                }
+            }
+            });
+
+            new Chart(document.getElementById("chartAmbientHum"), {
+            type: 'line',
+            data: {
+                labels: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['timestamp'];
+                    }, $data));
+                    ?>,
+                datasets: [
+                    {
+                        label: "P5",
+                        data: <?php
+                            echo json_encode(array_map(function ($p)
+                            {
+                                return $p['json']['AHP5'];
+                            }, $data));
+                    ?>,
+                        fill: '2',
+                        borderColor: "red",
+                        backgroundColor: "rgba(179,181,198,0.5)"
+                    },
+                    {
+                        label: "P50",
+                        data: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['json']['AHP50'];
+                    }, $data));
+                    ?>,
+                        borderColor: "green",
+                        backgroundColor: "rgba(26,181,53,0.5)",
+                        fill: false
+                    },
+                    {
+                        label: "P95",
+                        data: <?php
+                    echo json_encode(array_map(function ($p)
+                    {
+                        return $p['json']['AHP95'];
+                    }, $data));
+                    ?>,
+                        borderColor: "blue",
+                        backgroundColor: "rgba(27,42,198,0.5)",
+                        fill: false
+                    }
+                ]
+            },
+            options: {
+                legend: { display: true },
+                title: {
+                    display: true,
+                    text: 'Luftfuktighet'
                 },
                 elements: {
                     line: {
