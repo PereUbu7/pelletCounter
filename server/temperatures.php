57ab79d0068ec883970a246576fa97f40cd9622c
Martin Noring <martin.noring@gmail.com>
2024-02-29 21:43:50 +0100

    clean up temp charts

--- server/temperatures.php
+++ server/temperatures.php
@@ -288,68 +288,6 @@
                     ?>,
                 datasets: [
                     {
-                        label: "P5",
-                        data: <?php
-                            echo json_encode(array_map(function ($p)
-                            {
-                                return $p['json']['DS'][2]['P5'];
-                            }, $data));
-                    ?>,
-                        fill: '2',
-                        borderColor: "red",
-                        backgroundColor: "rgba(179,181,198,0.5)"
-                    },
-                    {
-                        label: "P50",
-                        data: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['json']['DS'][2]['P50'];
-                    }, $data));
-                    ?>,
-                        borderColor: "green",
-                        backgroundColor: "rgba(26,181,53,0.5)",
-                        fill: false
-                    },
-                    {
-                        label: "P95",
-                        data: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['json']['DS'][2]['P95'];
-                    }, $data));
-                    ?>,
-                        borderColor: "blue",
-                        backgroundColor: "rgba(27,42,198,0.5)",
-                        fill: false
-                    }
-                ]
-            },
-            options: {
-                legend: { display: true },
-                title: {
-                    display: true,
-                    text: 'Temperatur 2'
-                },
-                elements: {
-                    line: {
-                        tension: 0
-                    }
-                }
-            }
-            });
-
-            new Chart(document.getElementById("Temp3"), {
-            type: 'line',
-            data: {
-                labels: <?php
-                    echo json_encode(array_map(function ($p)
-                    {
-                        return $p['timestamp'];
-                    }, $data));
-                    ?>,
-                datasets: [
-                    {
                         label: "P5 1",
                         data: <?php
                             echo json_encode(array_map(function ($p)
@@ -452,7 +390,7 @@
                         data: <?php
                             echo json_encode(array_map(function ($p)
                             {
-                                return $p['json']['DS'][2]['P5'] - $p['json']['DS'][4]['P5'];
+                                return $p['json']['DS'][2]['P5'] - $p['json']['DS'][4]['P95'];
                             }, $data));
                     ?>,
                         fill: '2',
@@ -476,7 +414,7 @@
                         data: <?php
                     echo json_encode(array_map(function ($p)
                     {
-                        return $p['json']['DS'][2]['P95'] - $p['json']['DS'][4]['P95'];
+                        return $p['json']['DS'][2]['P95'] - $p['json']['DS'][4]['P5'];
                     }, $data));
                     ?>,
                         borderColor: "blue",
