156fb5385c4d4676aec4309756f24e29138bf6b1
Martin Noring <martin.noring@gmail.com>
2024-03-05 22:14:02 +0100

    print more debug

--- server/bucketReduction.php
+++ server/bucketReduction.php
@@ -8,6 +8,7 @@ class BucketReduction
     {
         return array_map(function ($key) use ($arr, $transform)
         { 
+            echo json_encode($arr[$key]);
            ArrayReduction::Mean($arr[$key], $transform); 
         }, 
         array_keys($arr));
