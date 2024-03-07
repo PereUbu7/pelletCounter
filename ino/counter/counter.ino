5d93953bd76703f2cc521fcd9cfc901c81b8a77b
Martin Noring <martin.noring@gmail.com>
2024-03-07 21:36:25 +0100

    better networking and measurements in pulse counter

--- ino/counter/counter.ino
+++ ino/counter/counter.ino
@@ -3,59 +3,138 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 
+// #define DEBUG 1
+
+uint32_t connectedAt;
+uint32_t startupAt;
+bool firstMeasure = true;
+
 void setup()
 {
+  #ifdef DEBUG
+  Serial.begin(9600);
+  #endif
+
+  // delay(500);
   const char *ssid = STASSID;
   const char *password = STAPSK;
 
-  IPAddress ip(10, 42, 0, 1);
-  IPAddress dns(0, 0, 0, 0);
-  IPAddress gateway(255, 255, 255, 0);
 
-  const char *host = "10.42.0.1";
-  const uint16_t port = 80;
-  const uint8_t channel = 11;
-  uint8_t bssid[6] = {0, 26, 115, 173, 217, 195};
 
-  uint32_t startupAt = millis();
+  IPAddress staticIP(192, 168, 0, 108);
+  IPAddress gateway(192, 168, 0, 108);
+  IPAddress netmask(255, 255, 255, 0);
 
+  startupAt = millis();
+
+  WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid);
+  WiFi.config(staticIP, gateway, netmask);
   WiFi.setAutoConnect(false);
+
+  #ifdef DEBUG
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  #endif
 
   uint32_t timeout = millis() + 30000;
   while ((WiFi.status() != WL_CONNECTED) && (millis() < timeout))
   {
+    #ifdef DEBUG
+    Serial.println("Waiting...");
+    #endif
     delay(50);
   }
 
-    Serial.begin(115200);
-
   if(millis() > timeout)
   {
+    #ifndef DEBUG
+    Serial.begin(9600);
+    #endif
     Serial.println("Timed out");
     return;
   }
 
-  uint32_t connectedAt = millis();
+ 
+}
+
+void loop() 
+{
+  if(!firstMeasure)
+  {
+    #ifdef DEBUG
+    Serial.print("waiting ");
+    Serial.println(connectedAt - startupAt);
+    #endif
+    delay(connectedAt - startupAt);
+  }
+  else
+  {
+    connectedAt = millis();
+    firstMeasure = false;
+  }
+
+  const char *host = "192.168.0.37";
+  const uint16_t port = 80;
+
 
   WiFiClient client;
 
+  #ifdef DEBUG
+  Serial.print("Connecting to ");
+  Serial.println(host);
+  #endif
+
   if (!client.connect(host, port))
   {
-    Serial.println("connection failed");
+    #ifndef DEBUG
+    Serial.begin(9600);
+    #endif
+    Serial.print("connection to ");
+    Serial.print(host);
+    Serial.print(":");
+    Serial.print(port);
+    Serial.println(" failed");
     return;
   }
 
   if (client.connected())
   {
-    client.println("GET /pelletCounter/server/api.php?version=1.1");
+    client.println("GET /pellet_call/ HTTP/1.1");
+    client.print("Host: ");
+    client.println(host);
+    client.println("Connection: close");
+    client.println();
+  }
+
+  auto timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 10000) {
+      #ifndef DEBUG
+      Serial.begin(9600);
+      #endif
+      Serial.println(">>> Client Timeout !");
+      return;
+    }
+  }
+
+  #ifndef DEBUG
+  Serial.begin(9600);
+  #endif
+  while(client.available())
+  {
+    char c = static_cast<char>(client.read());
+    Serial.print(c);
   }
 
   client.stop();
 
   uint32_t doneAt = millis();
 
+  #ifndef DEBUG
+  Serial.begin(9600);
+  #endif
 
   Serial.print("Connection time: ");
   Serial.println(connectedAt - startupAt);
@@ -64,19 +143,4 @@ void setup()
 
   Serial.print("At channel: ");
   Serial.print(WiFi.channel());
-  Serial.print(" and bssid: ");
-  memcpy( bssid, WiFi.BSSID(), 6 );
-  Serial.print(bssid[0]);
-  Serial.print(' ');
-  Serial.print(bssid[1]);
-  Serial.print(' ');
-  Serial.print(bssid[2]);
-  Serial.print(' ');
-  Serial.print(bssid[3]);
-  Serial.print(' ');
-  Serial.print(bssid[4]);
-  Serial.print(' ');
-  Serial.println(bssid[5]);
 }
-
-void loop() {}
