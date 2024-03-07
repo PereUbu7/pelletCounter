ddbf60912f620cf7f1ef647e78d9457c87509334
Martin Noring <martin.noring@gmail.com>
2024-03-07 21:34:17 +0100

    repair networking

--- ino/Hub/HubImpl.hpp
+++ ino/Hub/HubImpl.hpp
@@ -2,6 +2,7 @@
 #include "Utils.hpp"
 
 #include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
 
 template <size_t numberOfDS18B20s, size_t numberOfSamples, unsigned long secondsBetweenReads>
 State Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::Run()
@@ -29,6 +30,8 @@ void Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::RunHost()
 template <size_t numberOfDS18B20s, size_t numberOfSamples, unsigned long secondsBetweenReads>
 bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::StartServer()
 {
+  WiFi.persistent(false);
+
   WiFi.softAPConfig(config.APIP, config.APGateway, config.APSubnet);
   WiFi.softAP(config.ServerSSID);
 
@@ -38,6 +41,7 @@ bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::StartServer()
     Serial.println(config.ServerPelletEndpoint);
     
     _server.send(200, "text/html", "<h1>You are connected</h1>");
+
     secondsAtLastPelletPing = secs();
     ++pelletCounter; 
   });
@@ -74,6 +78,7 @@ bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::RunClient()
 template <size_t numberOfDS18B20s, size_t numberOfSamples, unsigned long secondsBetweenReads>
 bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::StartClient()
 {
+  WiFi.persistent(false);
   WiFi.mode(WIFI_STA);
   WiFi.begin(config.ClientSSID, config.ClientPassword);
   WiFi.setAutoConnect(false);
@@ -81,7 +86,7 @@ bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::StartClient()
   auto timeout = millis() + 30000ul;
   while ((WiFi.status() != WL_CONNECTED) && (millis() < timeout))
   {
-    // delay(500);// TODO: Remake without delay. Maybe a state: ClientConnecting
+    delay(500);// TODO: Remake without delay. Maybe a state: ClientConnecting
     Serial.print("Waiting...");
   }
 
@@ -115,11 +120,29 @@ bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::SendPelletsDat
   client.print("GET ");
   client.print(config.ClientPelletEndpoint);
   client.println(pelletCounter);
+  client.print("Host: ");
+  client.println(config.ClientConnectToHost);
+  client.println("Connection: close");
+  client.println();
 
   Serial.print("Sending ");
-  Serial.print(pelletCounter);
-  Serial.println(" number of pellet pulses.");
+  Serial.print(config.ClientPelletEndpoint);
+  Serial.println(pelletCounter);
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 30000) {
+      Serial.println(">>> Client Timeout !");
+      return false;
+    }
+  }
   
+  while(client.available())
+  {
+    char c = static_cast<char>(client.read());
+    Serial.print(c);
+  }
+
   client.stop();
 
   pelletCounter = 0;
@@ -130,21 +153,23 @@ template <size_t numberOfDS18B20s, size_t numberOfSamples, unsigned long seconds
 bool Hub<numberOfDS18B20s, numberOfSamples, secondsBetweenReads>::SendSensorData()
 {
   WiFiClient client;
+  HTTPClient http;
 
-  if (!client.connect(config.ClientConnectToHost, config.ClientConnectToPort))
-  {
-    Serial.println("sensor client connection failed");
-    return false;
-  }
+  http.begin(client, config.ClientSensorEndpoint);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  auto json = _reader.GetData().toJson();
+  
+  String httpRequestData = String("version=1.1&type=furnceRoomSensors&json=") + String(json.c_str());  
 
-  if (!client.connected()) { return false; }
+  Serial.print("POST: ");
+  Serial.println(httpRequestData);         
 
-  client.print("GET ");
-  client.print(config.ClientSensorEndpoint);
-  client.println(_reader.GetData().toJson().c_str());
-  client.stop();
+  int httpResponseCode = http.POST(httpRequestData);
+  http.writeToStream(&Serial);
+  http.end();
 
-  Serial.println("Sending sensor data");
+  if(httpResponseCode != 200) { return false; }
 
   _reader.ClearBuffer();
   sensorBufferIsFull = false;
