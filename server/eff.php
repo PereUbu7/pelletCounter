e463740a9b0cc00dd03665a63168955ff13e2b2d
Martin Noring <martin.noring@gmail.com>
2025-12-10 11:33:17 +0100

    changed to array_map

--- server/eff.php
+++ server/eff.php
@@ -41,12 +41,12 @@
 
     $manualValues = $manualRepo->getValues($autoValues);
 
-    $consumptionValues = BucketReduction::Mean($autoValues, function ($item) 
+    $consumptionValues = array_map(function ($item) 
                             { 
                                 return (($item['DS'][2]['P50'] - $item['DS'][4]['P50'])*2700 + /* dT * 2700 l/h */ 
                                         ($item['DS'][3]['P50'] - $item['DS'][1]['P50'])*410) * /* dT * 410 l/h */
                                         4186 / 3600000; // Cp_water 4186 J/kg/K -> kW
-                            });
+                            }, $autoValues);
 
     # Map number of pulses to manual records
     for ($i = 0; $i < count($manualValues) - 1; ++$i) 
